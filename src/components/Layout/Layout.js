9b5bca1ece80f5ccb4f8e204a7db21a400e535fb
Mizanali Panjwani <mizanalip786@gmail.com>
2020-12-12 03:21:35 +0530

    added BurgerBuilder and Layout components

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Aux from '../../hoc/Auxiliary'
+import './Layout.css'
+
+const layout = (props) => {
+    return(
+        <Aux>
+            <div className="header">Toolbar, SideDrawer, Backdrop</div>
+            <main className="content">
+                {props.children}
+            </main>
+        </Aux>
+    );
+}
+
+export default layout
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Auxiliary'
+
+class BurgerBuilder extends Component {
+    // constructor() {
+    //     this.state
+    // }
+    render() {
+        return (
+            <Aux>
+                <div>Burger Pic</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder
