f20f8af1a241e0bbfc32cee2874e0e6091bcd5f1
Mizanali Panjwani <mizanalip786@gmail.com>
2020-12-17 05:44:37 +0530

    added lifecycle hook in modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Modal.css'
 import Aux from '../../../hoc/Auxiliary'
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => {
-    return (
-        <Aux>
-            <Backdrop show={props.show} backdropClicked={props.modalClosed} />
-            <div className="Modal" style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'}}>
-                {props.children}
-            </div>
-        </Aux>
-    );
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show //rerender the Modal, and, consequently, the wrapped OrderSummary component only if the show prop changes
+    }
+
+    componentDidUpdate() {
+        console.log('Modal did update');
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} backdropClicked={this.props.modalClosed} />
+                <div className="Modal" style={{transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: this.props.show ? '1' : '0'}}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
 }
 
 export default Modal
