80b8338428a9935b8c7163265080ba98d63db442
Mizanali Panjwani <mizanalip786@gmail.com>
2021-03-09 00:32:46 +0530

    Added some basic tests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,7 @@ import * as actions from '../../store/actions'
 
 // let ingredientPrices = null;
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     constructor(props) {
         super(props);
         this.state = {
